bab6191b4e5e96c9a676e757288021c7074fb16f
Silvio Mori Neto <silviomori@gmail.com>
2021-06-19 13:34:43 -0300

    Create public endpoint to findAll cities

---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.bds04.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.repositories.CityRepository;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository cityRepository;
+
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll() {
+		return cityRepository.findAll(Sort.by("name")).stream().map(entity -> new CityDTO(entity))
+				.collect(Collectors.toList());
+	}
+
+}
